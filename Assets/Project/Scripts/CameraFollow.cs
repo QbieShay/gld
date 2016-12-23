7e24ad568e16abaa822991ceef8b3defde02fb2c
Ilaria Cislaghi <ilaria.cislaghi@mail.polimi.it>
2016-12-23 09:21:22 +0100

    cave mesh with basic test character.

---
+++ Assets/Project/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject toFollow;
+	public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = toFollow.transform.position + offset;
+	}
+}

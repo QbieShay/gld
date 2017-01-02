dc88b4074c7b156dda23990f2a06462222c0753e
GiuseppeLozioEllena <giuseppe.ellena@studenti.unimi.it>
2017-01-02 12:48:46 +0100

    update cave scene with stalagmite and kart and test navMesh with a stormtrooper model

---
+++ Assets/Project/Scripts/EnemyAttack.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+	// Use this for initialization
+    public int diceInitiative;
+    public int minimunRoll;
+    int probability;
+    public int meleDistance;
+    NavMeshAgent nav;
+
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (nav.remainingDistance <= nav.stoppingDistance)
+        {
+            probability = Random.Range(1, diceInitiative);
+            Debug.Log(probability);
+            if (probability > minimunRoll && nav.remainingDistance < meleDistance)
+            {
+                Debug.Log("Attack melee");
+
+            }
+            else
+            if (probability > minimunRoll && nav.remainingDistance > meleDistance)
+            {
+                Debug.Log("Attack ranged");
+
+            }
+        }
+    }
+}

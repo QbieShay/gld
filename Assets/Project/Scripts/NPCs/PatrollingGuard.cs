2917895fdde63ab530cae46e697f988afe3cd825
Federico Aliprandi <fede.alip@gmail.com>
2017-01-12 15:50:36 +0100

    Extracted obstacle avoidance logic into ObstacleAvoidance class

--- Assets/Project/Scripts/NPCs/PatrollingGuard.cs
+++ Assets/Project/Scripts/NPCs/PatrollingGuard.cs
@@ -20,12 +20,7 @@ public class PatrollingGuard : MonoBehaviour
     }
 
     public WaypointTime[] path;
-    public float walkingSpeed;
     public float waypointReachedThreshold = 0.1f;
-    public float steering = 60;
-    public float steeringMaxAvoidObstacle = 45;
-    public float steeringMinAvoidObstacle = 30;
-    public float angleReachedThreshold = 3;
     public GameObject spawnOnDefeated;
     public float lookCarefullyDuration = 3;
 
@@ -42,12 +37,14 @@ public class PatrollingGuard : MonoBehaviour
     private CharacterController characterController;
     private Animator animator;
     private NoiseListener noiseListener;
+    private ObstacleAvoidance obstacleAvoidance;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         noiseListener = GetComponent<NoiseListener>();
+        obstacleAvoidance = GetComponent<ObstacleAvoidance>();
 
         currentDestination = transform.position;
         // if there are no waypoints, automatically add one at NPC's position
@@ -270,7 +267,7 @@ public class PatrollingGuard : MonoBehaviour
 
     private void ActionMoveTowardsWaypoint()
     {
-        MoveTowardsPointAvoidingObstacles(currentDestination);        
+        obstacleAvoidance.MoveTowardsPointAvoidingObstacles(currentDestination);        
     }
 
     private void ActionStopMovingTowardsWaypoint()
@@ -319,7 +316,7 @@ public class PatrollingGuard : MonoBehaviour
 
     private void ActionMoveTowardsNoiseSource()
     {
-        MoveTowardsPointAvoidingObstacles(noiseSourcePosition);
+        obstacleAvoidance.MoveTowardsPointAvoidingObstacles(noiseSourcePosition);
     }
 
     private void ActionStartLookingCarefully()
@@ -346,109 +343,9 @@ public class PatrollingGuard : MonoBehaviour
         killed = true;
     }
 
-    /// <summary>
-    /// Checks with raycasts for colliders in the "Walls" layer in the given direction.
-    /// 5 rays are cast, in the following order: very left/right, slightly left/right, center.
-    /// The first ray that hits returns the value.
-    /// </summary>
-    /// <param name="direction">Direction vector: it MUST be normalized (i.e. magnitude 1)!</param>
-    /// <returns>Returns null if not collisions detected, -2 or 2 if a very left
-    /// or very right ray hit, -1 or 1 if a slightly left or right ray hit,
-    /// 0 if the center ray hit.</returns>
-    private int? CheckForWalls(Vector3 direction)
-    {
-        float centerRayLength = 3;
-        float slightlyOffsetRayLength = 2;
-        float slightlyOffsetRayAngle = 10;
-        float veryOffsetRayLength = 1;
-        float veryOffsetRayAngle = 30;
+    
 
-        Color c = Color.green;
-        bool hit = false;
-
-        // very-left ray
-        hit = Physics.Raycast(transform.position, Quaternion.Euler(0, -veryOffsetRayAngle, 0) * direction, veryOffsetRayLength, LayerMask.GetMask("Walls"));
-        c = hit ? Color.red : Color.green;
-        Debug.DrawLine(transform.position, transform.position + Quaternion.Euler(0, -veryOffsetRayAngle, 0) * direction * veryOffsetRayLength, c, 1);
-        if (hit)
-        {
-            return -2;
-        }
-
-        // very-right ray
-        hit = Physics.Raycast(transform.position, Quaternion.Euler(0, veryOffsetRayAngle, 0) * direction, veryOffsetRayLength, LayerMask.GetMask("Walls"));
-        c = hit ? Color.red : Color.green;
-        Debug.DrawLine(transform.position, transform.position + Quaternion.Euler(0, veryOffsetRayAngle, 0) * direction * veryOffsetRayLength, c, 1);
-        if (hit)
-        {
-            return 2;
-        }
-
-        // slightly-left ray
-        hit = Physics.Raycast(transform.position, Quaternion.Euler(0, -slightlyOffsetRayAngle, 0) * direction, slightlyOffsetRayLength, LayerMask.GetMask("Walls"));
-        c = hit ? Color.red : Color.green;
-        Debug.DrawLine(transform.position, transform.position + Quaternion.Euler(0, -slightlyOffsetRayAngle, 0) * direction * slightlyOffsetRayLength, c, 1);
-        if (hit)
-        {
-            return -1;
-        }
-
-        // slightly-right ray
-        hit = Physics.Raycast(transform.position, Quaternion.Euler(0, slightlyOffsetRayAngle, 0) * direction, slightlyOffsetRayLength, LayerMask.GetMask("Walls"));
-        c = hit ? Color.red : Color.green;
-        Debug.DrawLine(transform.position, transform.position + Quaternion.Euler(0, slightlyOffsetRayAngle, 0) * direction * slightlyOffsetRayLength, c, 1);
-        if (hit)
-        {
-            return 1;
-        }
-
-        // center ray
-        hit = Physics.Raycast(transform.position, direction, centerRayLength, LayerMask.GetMask("Walls"));
-        c = hit ? Color.red : Color.green;
-        Debug.DrawLine(transform.position, transform.position + direction * centerRayLength, c, 1);
-        if (hit)
-        {
-            return 0;
-        }
-
-        return null;
-    }
-
-    private void MoveTowardsPointAvoidingObstacles(Vector3 destination)
-    {
-        Vector3 destDirection = destination - transform.position;
-        destDirection.y = 0;
-        destDirection.Normalize();
-        int? hit = CheckForWalls(transform.forward);
-        if (hit != null)
-        {
-            float rotation = 0;
-            if (hit == -2)
-                rotation = steeringMaxAvoidObstacle;
-            else if (hit == -1 || hit == 0)
-                rotation = steeringMinAvoidObstacle;
-            else if (hit == 1)
-                rotation = -steeringMinAvoidObstacle;
-            else if (hit == 2)
-                rotation = -steeringMaxAvoidObstacle;
-            transform.Rotate(new Vector3(0, rotation * Time.deltaTime, 0));
-        }
-        else
-        {
-            Vector3 lookPos = destination - transform.position;
-            lookPos.y = 0;
-            Quaternion targetRotation = Quaternion.LookRotation(lookPos);
-            float angleDifference = Mathf.DeltaAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y);//(targetRotation.eulerAngles - transform.eulerAngles).y;
-            float sign = Mathf.Sign(angleDifference);
-            if (Mathf.Abs(angleDifference) > angleReachedThreshold)
-            {
-                transform.Rotate(new Vector3(0, sign * steering * Time.deltaTime, 0));
-            }
-        }
-        characterController.SimpleMove(transform.forward * walkingSpeed);
-        if (animator.GetFloat("Forward") < 0.5f)
-            animator.SetFloat("Forward", 0.5f);
-    }
+    
 
     private IEnumerator LookCarefully()
     {

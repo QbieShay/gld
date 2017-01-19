6d54a2971f2f6e98da4afb2dd443628494e41d4f
GiuseppeLozioEllena <giuseppe.ellena@studenti.unimi.it>
2017-01-19 16:39:57 +0100

    implement ai heavyguard meccanics

---
+++ Assets/Project/Scripts/HeavyGuard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyGuard : ApproachPlayer
+{
+    ObstacleAvoidance obstacleadvoidance;
+    NpcBehaviour behaviour;
+    bool isApproach=false;
+    public Transform target;
+    public float treshold;
+
+
+    public override void Approach()
+    {
+        if(!isApproach)
+           isApproach = true;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        obstacleadvoidance = GetComponent<ObstacleAvoidance>();
+        behaviour = GetComponent<NpcBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isApproach)
+        {
+            if (behaviour.behaviour == "Approach")
+            {
+           
+                if (Vector3.Distance(transform.position, target.position) > treshold)
+                {
+                    Debug.Log("approach");
+                    obstacleadvoidance.MoveTowardsPointAvoidingObstacles(target.position);
+                }
+                else
+                    isApproach = false;
+            }
+            else
+            {
+                isApproach = false;
+            }
+        }
+        
+
+	}
+}
